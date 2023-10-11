cdde52359120bc86aa1550707dbd3ec7d990b880
Vadim Afanasev <trim-agency@yandex.ru>
2023-10-11 22:22:02 +0700

    Продолжаю писать тесты

--- CoffeeMachine.Tests/GlobalUsings.cs
+++ CoffeeMachine.Tests/GlobalUsings.cs
@@ -1,9 +1,10 @@
-global using NUnit.Framework;
 global using CoffeeMachine.Common;
-global using CoffeeMachine.Models;
+global using CoffeeMachine.Common.Interfaces;
+global using CoffeeMachine.Dto;
 global using CoffeeMachine.Models.Data;
-
+global using CoffeeMachine.Services;
+global using FluentAssertions;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
-global using FluentAssertions;
+global using NUnit.Framework;
 global using NUnit.Framework.Internal;
